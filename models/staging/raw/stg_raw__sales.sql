8585fb0d2a7c7c1eb280f6dd5a5f26ffd804f207
Hector Del Rosario Bautista <hectorhdelrosario07@gmail.com>
2024-12-07 14:25:33 +0000

    Add stg_raw__sales model for staging sales data

---
+++ models/staging/raw/stg_raw__sales.sql
@@ -0,0 +1,24 @@
+with 
+
+source as (
+
+    select * from {{ source('raw', 'sales') }}
+
+),
+
+renamed as (
+
+    select
+        date_date,
+        orders_id,
+        pdt_id,
+        revenue,
+        quantity
+
+    from source
+
+)
+
+select * from renamed
+
+dbt run --select stg_raw__sales
